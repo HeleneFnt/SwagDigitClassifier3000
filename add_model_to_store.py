b1bd7d8d4e4edea2b5eebe91b015913a5d25570f
LNWatta <helene.finot@le-campus-numerique.fr>
2025-03-12 10:02:05 +0100

    feat: add model via script

--- add_model_to_store.py
+++ add_model_to_store.py
@@ -1,22 +1,25 @@
 from modelstore import ModelStore
-import os
 import pickle
+import numpy as np
+import setuptools
+import os
 
-
+# Patch NumPy 2.0 : define np.float_ if not exist to fix this AttributeError: `np.float_` was removed in the NumPy 2.0 release. Use `np.float64` instead
+if not hasattr(np, 'float_'):
+    np.float_ = np.float64
 
 # Define stockage directory
 MODEL_DIR = "models"
 os.makedirs(MODEL_DIR, exist_ok=True)
 
 # Initiate ModelStore with a local storage
-model_store = ModelStore.from_file_system(MODEL_DIR)
+model_store = ModelStore.from_file_system(root_directory=MODEL_DIR)
 
 # Load model
-model_path = "digit_classifier.pkl"
-with open(model_path, 'rb') as f:
+with open("digit_classifier.pkl", 'rb') as f:
     model = pickle.load(f)
-print(type(model))
+# print(type(model))
 
 # Save model in  modelstore
-meta_data = model_store.upload("digit_classifier", ) # TO DO
+meta_data = model_store.sklearn.upload("other", model = model, model_id="test")
 print(f"Model sucessfully load! MetaData : {meta_data}")
